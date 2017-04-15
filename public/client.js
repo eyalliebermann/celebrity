c914cb805ae3b5b82c0c55305aa99714a77f9488
eyalliebermann <eyal.liebermann@gmail.com>
2017-04-15 18:40:58 +0300

    take from server

--- public/client.js
+++ public/client.js
@@ -1,23 +1,23 @@
 var celebrities = [{
         name: 'Madonna',
         image: 'http://static.deathandtaxesmag.com/uploads/2015/04/madonna-do-or-dont-list.jpg',
-        description: 'An American singer, songwriter, actress, and businesswoman'
+        description: 'An American singer, songwriter, actress, and businesswoman (local)'
     },
 
     {
         name: 'Arnold Schwarzenegger',
         image: 'https://metrouk2.files.wordpress.com/2016/04/arnie-2.jpg',
-        description: 'An Austrian-American actor, producer, businessman, investor, author, philanthropist, activist, politician and former professional bodybuilder'
+        description: 'An Austrian-American actor, producer, businessman, investor, author, philanthropist, activist, politician and former professional bodybuilder(local)'
     },
     {
         name: 'Barack Obama',
         image: 'https://www.fraudswatch.com/wp-content/uploads/2016/04/Barack-Hussein-Obama-1.jpg?w=640',
-        description: 'An americn politician'
+        description: 'An americn politician (local)'
     },
     {
         name: 'Alan Turing',
         image: 'https://www.biography.com/.image/t_share/MTE5NDg0MDU1MTUzMTE2Njg3/alan-turing-9512017-1-402.jpg',
-        description: 'A British mathematician'
+        description: 'A British mathematician(local)'
     }
 ];
 $(document)
@@ -25,6 +25,10 @@ $(document)
         function () {
             var i = 0;
             var interval = null;
+
+            celebrities = $.getJSON('/api/celebs', function(data) {
+                celebrities = data;
+            });
 
             $('button').click(function () {
                 loadNewCelebrity();
