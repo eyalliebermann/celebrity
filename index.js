b878534329f2b2bf56240cfec5c1a398ce5bdd61
eyalliebermann <eyal.liebermann@gmail.com>
2017-04-15 17:26:13 +0300

    serving celebs from server

--- index.js
+++ index.js
@@ -5,8 +5,33 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(app.get('port'), function() {
+app.get('/api/celebs', function (req, res) {
+  res.status(200).json(CELEBRITIES);
+});
+
+app.listen(app.get('port'), function () {
   console.log('Node app is running on port', app.get('port'));
 });
 
+var CELEBRITIES = [{
+    name: 'Madonna',
+    image: 'http://static.deathandtaxesmag.com/uploads/2015/04/madonna-do-or-dont-list.jpg',
+    description: 'An American singer, songwriter, actress, and businesswoman'
+  },
 
+  {
+    name: 'Arnold Schwarzenegger',
+    image: 'https://metrouk2.files.wordpress.com/2016/04/arnie-2.jpg',
+    description: 'An Austrian-American actor, producer, businessman, investor, author, philanthropist, activist, politician and former professional bodybuilder'
+  },
+  {
+    name: 'Barack Obama',
+    image: 'https://www.fraudswatch.com/wp-content/uploads/2016/04/Barack-Hussein-Obama-1.jpg?w=640',
+    description: 'An americn politician'
+  },
+  {
+    name: 'Alan Turing',
+    image: 'https://www.biography.com/.image/t_share/MTE5NDg0MDU1MTUzMTE2Njg3/alan-turing-9512017-1-402.jpg',
+    description: 'A British mathematician'
+  }
+];
