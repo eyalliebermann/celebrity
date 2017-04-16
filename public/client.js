5419456f2c3eab1a2a919d6d97dd72342ee5d54d
eyalliebermann <eyal.liebermann@gmail.com>
2017-04-16 13:16:45 +0300

    add fittext

--- public/client.js
+++ public/client.js
@@ -1,32 +1,11 @@
-var celebrities = [{
-        name: 'Madonna',
-        image: 'http://static.deathandtaxesmag.com/uploads/2015/04/madonna-do-or-dont-list.jpg',
-        description: 'An American singer, songwriter, actress, and businesswoman (local)'
-    },
 
-    {
-        name: 'Arnold Schwarzenegger',
-        image: 'https://metrouk2.files.wordpress.com/2016/04/arnie-2.jpg',
-        description: 'An Austrian-American actor, producer, businessman, investor, author, philanthropist, activist, politician and former professional bodybuilder(local)'
-    },
-    {
-        name: 'Barack Obama',
-        image: 'https://www.fraudswatch.com/wp-content/uploads/2016/04/Barack-Hussein-Obama-1.jpg?w=640',
-        description: 'An americn politician (local)'
-    },
-    {
-        name: 'Alan Turing',
-        image: 'https://www.biography.com/.image/t_share/MTE5NDg0MDU1MTUzMTE2Njg3/alan-turing-9512017-1-402.jpg',
-        description: 'A British mathematician(local)'
-    }
-];
 $(document)
     .ready(
         function () {
             var i = 0;
             var interval = null;
 
-            celebrities = $.getJSON('/api/v1/celebs', function(data) {
+            var celebrities = $.getJSON('/api/v1/celebs', function(data) {
                 celebrities = data;
             });
 
@@ -50,6 +29,7 @@ $(document)
                 }, 1000);
                 var celebrity = celebrities[i];
                 $('#name').html(celebrity.name);
+                $('#name').fitText();
                 $('img').attr('src', celebrity.image);
                 $('img').attr('alt', celebrity.name + "s picture");
                 $('#description').html(celebrity.description);
