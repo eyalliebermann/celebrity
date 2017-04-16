e2e12a37cb0f4bcfd171abccb31aeb8e60b6a4fc
eyalliebermann <eyal.liebermann@gmail.com>
2017-04-16 20:41:14 +0300

    post testing plan

--- test/server.test.js
+++ test/server.test.js
@@ -7,21 +7,34 @@ describe('Getting celebritites', function () {
     var url = '/api/v1/celebs/'
     it('returns statu 200', function (done) {
         request
-        .get(url)
-        .then( function (res) {
-            expect(res.statusCode).to.equal(200);
-            done();
-        });
+            .get(url)
+            .then(function (res) {
+                expect(res.statusCode).to.equal(200);
+                done();
+            });
     });
 
     it('returns a valid json', function (done) {
         request
-        .get(url)
-        .then( function (res) {
-            expect(res.body).to.be.json;
-            expect(res.body).length.to.be.greaterThan(0);
-            expect(res.body[0].name).to.match(/^[a-zA-Z]*[a-zA-Z ]*[a-zA-Z]*$/);
-            done();
-        });
+            .get(url)
+            .then(function (res) {
+                expect(res.body).to.be.json;
+                expect(res.body).length.to.be.greaterThan(0);
+                expect(res.body[0].name).to.match(/^[a-zA-Z]*[a-zA-Z ]*[a-zA-Z]*$/);
+                done();
+            });
     });
+});
+
+describe('Adding item', function () {
+    it.skip('rejects empty item', function (done) {});
+    it.skip('rejects a non english phrase', function (done) {});
+    it.skip('rejets duplicate item', function (done) {});
+    it.skip('rejects duplicate item the differs on casing', function (done) {});
+    it.skip('it rejects the Nth item > 1K', function (done) {});
+    it.skip('it rejects a too long item', function (done) {});
+    it.skip('it reject a too many words item', function (done) {});
+    it.skip('accepts a valid item', function (done) {});
+    it.skip('grows the array in one', function (done) {});
+    it.skip('adds the item on the end', function (done) {});
 });
