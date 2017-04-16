c486121829d62b8db9005de4de86f6f9f8824f15
eyalliebermann <eyal.liebermann@gmail.com>
2017-04-16 20:49:03 +0300

    identation

--- test/server.test.js
+++ test/server.test.js
@@ -3,7 +3,7 @@ const app = require('../server');
 
 const request = require('supertest')(app);
 
-describe('Getting celebritites',  () => {
+describe('Getting celebritites', () => {
     var url = '/api/v1/celebs/'
     it('returns statu 200', (done) => {
         request
@@ -17,7 +17,7 @@ describe('Getting celebritites',  () => {
     it('returns a valid json', (done) => {
         request
             .get(url)
-            .then( (res) => {
+            .then((res) => {
                 expect(res.body).to.be.json;
                 expect(res.body).length.to.be.greaterThan(0);
                 expect(res.body[0].name).to.match(/^[a-zA-Z]*[a-zA-Z ]*[a-zA-Z]*$/);
@@ -26,15 +26,15 @@ describe('Getting celebritites',  () => {
     });
 });
 
-describe('Adding item',  () => {
-    it.skip('rejects empty item', function (done) {});
-    it.skip('rejects a non english phrase', function (done) {});
-    it.skip('rejets duplicate item', function (done) {});
-    it.skip('rejects duplicate item the differs on casing', function (done) {});
-    it.skip('it rejects the Nth item > 1K', function (done) {});
-    it.skip('it rejects a too long item', function (done) {});
-    it.skip('it reject a too many words item', function (done) {});
-    it.skip('accepts a valid item', function (done) {});
-    it.skip('grows the array in one', function (done) {});
-    it.skip('adds the item on the end', function (done) {});
+describe('Adding item', () => {
+    it.skip('rejects empty item', (done) => {});
+    it.skip('rejects a non english phrase', (done) => {});
+    it.skip('rejets duplicate item', (done) => {});
+    it.skip('rejects duplicate item the differs on casing', (done) => {});
+    it.skip('it rejects the Nth item > 1K', (done) => {});
+    it.skip('it rejects a too long item', (done) => {});
+    it.skip('it reject a too many words item', (done) => {});
+    it.skip('accepts a valid item', (done) => {});
+    it.skip('grows the array in one', (done) => {});
+    it.skip('adds the item on the end', (done) => {});
 });
