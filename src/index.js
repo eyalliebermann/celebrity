6b8f3ece5a4a3a3f8ef1638ce775468bde5c6f9b
eyalliebermann <eyal.liebermann@gmail.com>
2017-04-17 20:35:35 +0300

    react single file demo

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-document.write(require('./content.js'));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <h1>Hello React. Let's see how can Travis and heroku handle you</h1>,
+  document.getElementById('root')
+);
